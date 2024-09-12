d2036105122d218b667a2b3f9f46682e91d5bdfa
Volnei <Volnei.Foss@townsq.com.br>
2024-09-12 12:04:07 -0300

    created file to create my ui list

---
+++ ServiceLogs/ServiceLogs/ui/list/ServiceLogsListView.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceLogsListView.swift
+//  ServiceLogs
+//
+//  Created by Volnei Foss on 12/09/24.
+//
+
+import SwiftUI
+
+struct ServiceLogsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ServiceLogsListView()
+}
