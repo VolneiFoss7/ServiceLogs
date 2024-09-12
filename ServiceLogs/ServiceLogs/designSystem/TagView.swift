218bef3c60d1b81bb65b4ae520e026f61cb19739
Volnei <Volnei.Foss@townsq.com.br>
2024-09-12 12:37:44 -0300

    including two new pictures to using un my mock data

---
+++ ServiceLogs/ServiceLogs/designSystem/TagView.swift
@@ -0,0 +1,18 @@
+//
+//  TagView.swift
+//  ServiceLogs
+//
+//  Created by Volnei Foss on 12/09/24.
+//
+
+import SwiftUI
+
+struct TagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagView()
+}
