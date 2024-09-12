ff2f8d8e079335664c3afe95edb03fac3b668680
Volnei <Volnei.Foss@townsq.com.br>
2024-09-12 11:52:12 -0300

    project base

---
+++ ServiceLogs/ServiceLogs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ServiceLogs
+//
+//  Created by Volnei Foss on 12/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ServiceLogs/ServiceLogs/ServiceLogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceLogsApp.swift
+//  ServiceLogs
+//
+//  Created by Volnei Foss on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ServiceLogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
