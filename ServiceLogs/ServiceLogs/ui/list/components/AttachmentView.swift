fecf3ad80331dfcda4b1f008bc69e328f5b48554
Volnei <Volnei.Foss@townsq.com.br>
2024-09-12 13:16:37 -0300

    including new assets to using in my component

---
+++ ServiceLogs/ServiceLogs/ui/list/components/AttachmentView.swift
@@ -0,0 +1,18 @@
+//
+//  AttachmentView.swift
+//  ServiceLogs
+//
+//  Created by Volnei Foss on 12/09/24.
+//
+
+import SwiftUI
+
+struct AttachmentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttachmentView()
+}
